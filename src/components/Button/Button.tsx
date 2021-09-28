b9ac5b49f2e787c95319eef8ff25b7470d16ed77
Geraldo Istalin <geraldoboueres@gmail.com>
2021-09-28 09:48:27 -0300

    Storybook

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import * as B from './Button.styles'
+
+export interface ButtonProps { 
+
+}
+
+export default function Button ({}: ButtonProps){
+    return <B.Wrapper>
+        Botão Geraldo
+    </B.Wrapper>
+
+}
