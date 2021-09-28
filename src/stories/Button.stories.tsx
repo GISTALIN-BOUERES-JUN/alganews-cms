a77e86468cfdfcd6ac6cb3ffd850be9657c47040
Geraldo Istalin <geraldoboueres@gmail.com>
2021-09-28 10:21:29 -0300

    Style Button

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -6,32 +6,26 @@ import Button, { ButtonProps } from '../components/Button/Button'
 export default {
   title: 'Example/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
+  variant: 'primary',
   label: 'Button',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
+export const Danger = Template.bind({});
+Danger.args = {
+  variant: 'danger',
   label: 'Button',
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
+export const Text = Template.bind({});
+Text.args = {
+  variant: 'text',
   label: 'Button',
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
