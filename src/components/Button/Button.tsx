84071a5af7547cbb36dc23fc121be65df4d8c3ca
Geraldo Istalin <geraldoboueres@gmail.com>
2021-09-29 14:01:28 -0300

    Button Props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,17 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react'
 import * as B from './Button.styles'
 
-export interface ButtonProps { 
+export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>{ 
     variant: 'danger' | 'text' | 'primary'
     label: string
+    
 
 }
 
-export default function Button ({ label, variant }: ButtonProps){
-    return <B.Wrapper variant={variant}>
+export default function Button ({ label, variant, ...props }: ButtonProps){
+    return <B.Wrapper variant={variant} disabled={disabled}>
         {label}
     </B.Wrapper>
 
 }
+
